a2bbc4d8dd9144dd295fabbb928fd118d4e5d39d
Sirag Ali <siragalihot@hotmail.com>
2025-01-08 18:31:31 +0400

    this is my first change

--- ConsoleApp.RetirmentProject/Program.cs
+++ ConsoleApp.RetirmentProject/Program.cs
@@ -1,14 +1,20 @@
 ﻿//Declaration of variables
-string name = string.Empty;
+using System.Xml.Linq;
+
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirmentYear = 65;
 int retirmentRemainingYears = 0;
-Console.WriteLine("Please enter your name");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your firstname");
+firstName = Console.ReadLine();
+Console.WriteLine("Please enter your lastname");
+lastName = Console.ReadLine();
+
 Console.WriteLine("Please enter your age");
 age =Convert.ToInt32( Console.ReadLine());
 retirmentRemainingYears = retirmentYear - age;
-Console.WriteLine($"your name is {name}");
+Console.WriteLine($"your fullname is : {firstName} {lastName}");
 Console.WriteLine($"you have {retirmentRemainingYears} to retire");
 
 
