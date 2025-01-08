4b4471ddfc3554a5f400e5a39eeeec6a1d121683
User <User@DESKTOP-5J9HHU2>
2025-01-08 15:20:55 +0400

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is sirag Buraie");
+Console.WriteLine("I am Dotnet Developer");
+Console.WriteLine("I am from Sudan");
---
+++ ConsoleApp.RetirmentProject/Program.cs
@@ -0,0 +1,16 @@
+﻿//Declaration of variables
+string name = string.Empty;
+int age = 0;
+int retirmentYear = 65;
+int retirmentRemainingYears = 0;
+Console.WriteLine("Please enter your name");
+name = Console.ReadLine();
+Console.WriteLine("Please enter your age");
+age =Convert.ToInt32( Console.ReadLine());
+retirmentRemainingYears = retirmentYear - age;
+Console.WriteLine($"your name is {name}");
+Console.WriteLine($"you have {retirmentRemainingYears} to retire");
+
+
+
+
