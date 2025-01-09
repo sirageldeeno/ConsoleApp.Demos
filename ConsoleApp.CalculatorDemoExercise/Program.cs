1d3f79de29cc081b81995cef1a57d1af4ec397bb
Sirag Ali <siragalihot@hotmail.com>
2025-01-09 19:06:41 +0400

    calculatortuturial

---
+++ ConsoleApp.CalculatorDemoExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+string op;
+int result=0;
+Console.WriteLine("Welcome To Calculator App");
+Console.WriteLine("Enter Your FirstNumber");
+int firstNum=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Your SecondNumber");
+int SecondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Your operator:+/-/*//");
+op = Console.ReadLine();
+switch (op)
+{
+    case "+":
+        result = firstNum + SecondNum;
+        break;
+    case "-":
+        result = firstNum - SecondNum;
+        break;
+    case "*":
+        result = firstNum * SecondNum;
+        break;
+    case "/":
+        result = firstNum / SecondNum;
+        break;
+    default:
+        Console.WriteLine("Enter valid Operator");
+        break;
+
+}
+Console.WriteLine($"The Result is {result}");
+
+
+
